119c48f6f72f8ddaf0087981613bf3cd8c0903cb
mikael04 <mikael.coletto.en@gmail.com>
2021-07-28 11:16:35 -0300

    Adicionando dois exemplos de estrutura no shiny_app (shiny_protot_dashb e _navbar);

--- app_translation_mod.R
+++ app_translation_mod.R
@@ -1,16 +1,33 @@
 library(shiny)
 library(shiny.i18n)
 
-i18n <- Translator$new(translation_json_path = "dados/translations/translation.json")
-
-i18n$set_translation_language("fr")
+# File with translations
+i18n <- Translator$new(translation_csvs_path = "dados/translations/")
+i18n$set_translation_language("en") # here you select the default translation to display
 
 languageButton_UI <- function(id, i18n) {
   ns <- NS(id)
-  tagList(selectInput('Selecione ao idioma',
-                      i18n$t("Selecione o idioma"),
-                      choices = i18n$get_languages(),
-                      selected = i18n$get_key_translation())
+  fluidPage(
+    shiny.i18n::usei18n(i18n),
+    div(style = "float: right;",
+        selectInput('selected_language',
+                    i18n$t("Change language"),
+                    choices = i18n$get_languages(),
+                    selected = i18n$get_key_translation())
+    ),
+    titlePanel(i18n$t("Hello Shiny!"), windowTitle = NULL),
+    sidebarLayout(
+      sidebarPanel(
+        sliderInput("bins",
+                    i18n$t("Number of bins:"), # you use i18n object as always
+                    min = 1,
+                    max = 50,
+                    value = 30)
+      ),
+      mainPanel(
+        p(i18n$t("This is description of the plot."))
+      )
+    )
   )
 }
 
@@ -19,18 +36,7 @@ languageButton_Server <- function(id, global_session) {
     id,
     function(input, output, session) {
       ns <- NS(id)
-      observeEvent(input$go,{
-        print(input$go[1])
-        if((input$go[1] %% 2) != 0){
-          updateActionButton(session, "go",
-                             label = "Français")
-          update_lang(global_session, "en")
-        } else {
-          updateActionButton(session, "go",
-                             label = "English")
-          update_lang(global_session, "fr")
-        }
-      })
+      
     }
   )
 }
@@ -42,6 +48,14 @@ ui <- fluidPage(
 
 server <- function(input, output, session) {
   languageButton_Server("language_button", global_session = session)
+  
+  
+  observeEvent(input$selected_language, {
+    # This print is just for demonstration
+    print(paste("Language change!", input$selected_language))
+    # Here is where we update language in session
+    shiny.i18n::update_lang(session, input$selected_language)
+  })
 }
 
 shinyApp(ui, server)
