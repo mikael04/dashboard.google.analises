31bdf356fda92aafd5fd8f05e12a0d5d7806b1ff
mikael04 <mikael.coletto.en@gmail.com>
2021-08-06 10:59:00 -0300

    Removendo algumas coisas não mais usadas; Corrigindo traduções de filtros;

--- app_translation_mod.R
+++ app_translation_mod.R
@@ -3,7 +3,6 @@ library(shiny.i18n)
 
 # File with translations
 i18n <- Translator$new(translation_csvs_path = "dados/translations/")
-# i18n$set_translation_language("pt") # here you select the default translation to display
 
 languageButton_UI <- function(id, i18n) {
   ns <- NS(id)
@@ -24,11 +23,6 @@ languageButton_UI <- function(id, i18n) {
                     max = 50,
                     value = 30),
         selectInput("idPais", i18n$t(""), choices = c("")),
-        # selectInput("idPais2", i18n$t("Selecione o país:"), choices = c("Todos os países", "Brasil", "Argentina", "Chile", "Estados Unidos", "Uruguai") %>%
-        #               stats::setNames(c(i18n$t("Todos os países"), i18n$t("Brasil"), i18n$t("Argentina"),
-        #                                 i18n$t("Chile"), i18n$t("Estados Unidos"), i18n$t("Uruguai"))))
-        # choices=c('Transformers', 'Cable closets') %>% stats::setNames(nm = c(i18n$t('Transformers'), i18n$t('Cable closets'))),
-        
         selectInput("select", i18n$t("Escolha"), c("um", "dois", "três"))
         
       ),
@@ -62,35 +56,26 @@ server <- function(input, output, session) {
   r_aux <- reactiveVal(0)
   value <- reactiveVal(0)
   observeEvent(input$selected_language, {
-    browser()
-    if(value()){
-      browser()
+    lang <- ifelse(input$selected_language == "pt", "pt", "en")
+      # browser()
       # This print is just for demonstration
       print(paste("Language change!", input$selected_language))
+      print(paste("Language change lang!", lang))
       # Here is where we update language in session
-      shiny.i18n::update_lang(session, input$selected_language)
-      # if(input$selected_language == "pt"){
-      updateSelectInput(session, "idPais",
-                        label = i18n_r()$t("Selecione o país:"),
-                        choices = i18n_r()$t(c("Todos os países", "Brasil", "Argentina", "Chile", "Estados Unidos", "Uruguai")))
-      # choices = setNames(i18n$t("Todos os países"), i18n$t("Brasil"), i18n$t("Argentina"),
-      #                    i18n$t("Chile"), i18n$t("Estados Unidos"), i18n$t("Uruguai")))
-      # i18n
-      # i18n_r
-      # browser()
-      updateSelectInput(session, "select", label = i18n_r()$t("Escolha"),
-                        choices = i18n_r()$t(c("um", "dois", "três")))
-      updateSelectInput(session, "select", label = i18n_r()$t("Escolha"),
-                        choices = i18n_r()$t(c("um", "dois", "três")))
-      # updateSelectInput(session, "select", label = i18n_r()$t("Choose"),
-      #                   choices = i18n_r()$t(c("one", "two", "three")))
-      # }
-    }else{
-      value(1)
-      browser()
-    }
-    
+      shiny.i18n::update_lang(session, lang)
+      i18n_r()$set_translation_language(lang)
   })
+
+  observe({
+    updateSelectInput(session, "idPais",
+                      label = i18n_r()$t("Selecione o país:"),
+                      choices = i18n_r()$t(c("Todos os países", "Brasil", "Argentina", "Chile", "Estados Unidos", "Uruguai")))
+    updateSelectInput(session, "select", label = i18n_r()$t("Escolha"),
+                      choices = i18n_r()$t(c("um", "dois", "três")))
+  })
+      
+    
 }
 
 shinyApp(ui, server)
+
