db7ce9d72e957989a8e802900a70e9e27811d563
mikael04 <mikael.coletto.eng@gmail.com>
2021-08-20 09:34:20 -0300

    Adicionando arquivo para acessar bq;

---
+++ usando_bigquery.R
@@ -0,0 +1,25 @@
+library(tidyverse)
+library(dbplyr)
+library(bigrquery)
+library(DBI)
+
+
+
+con <- dbConnect(
+  bigrquery::bigquery(),
+  project = "cidacs-ai-covid19-br",
+  dataset = "backendDash",
+  billing = "cidacs-ai-covid19-br"
+)
+
+
+bigrquery::bq_auth() # aqui vai ter uma primeira configuração, depois ele usa um token que ele salva
+
+
+#df = tbl(con,"backend_dash_publi") banco raw
+df_filtros = tbl(con,"hml_base_filtro") #conexão com essa tabela, da para selecionar ou filtra direto com dplyr
+teste = df_filtros %>% collect() # download para dataframe
+paises = df_filtros %>% select(paises) %>% collect()
+glimpse(df_filtros)
+glimpse(teste)
+glimpse(paises)
