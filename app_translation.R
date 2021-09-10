a0ba2ecac9fd8ea1cead43ba2ed7487712216eb2
mikael04 <mikael.coletto.eng@gmail.com>
2021-09-10 10:01:06 -0300

    Tentando criar alterações para corrigir tradução;

--- app_translation.R
+++ app_translation.R
@@ -1,46 +1,30 @@
 #' This script demonstrates how to use shiny.i18n Translator object
-#' for live language change on the UI side. Two key steps are:
-#' (a) add `usei18n(i18n)` to UI
-#' (b) use `update_lang` function to change the language in session
+#' to change the language dynamically on the server side.
+#'
+#' Note that here we create a reactive object i18n in the server
+#' and rerender all the UI elements.
 
 library(shiny)
 library(shiny.i18n)
 
-# File with translations
-i18n <- Translator$new(translation_csvs_path = "dados/translations/")
-i18n$set_translation_language("en") # here you select the default translation to display
-
 ui <- fluidPage(
-  shiny.i18n::usei18n(i18n),
-  div(style = "float: right;",
-      selectInput('selected_language',
-                  i18n$t("Change language"),
-                  choices = i18n$get_languages(),
-                  selected = i18n$get_key_translation())
-  ),
-  titlePanel(i18n$t("Hello Shiny!"), windowTitle = NULL),
-  sidebarLayout(
-    sidebarPanel(
-      sliderInput("bins",
-                  i18n$t("Number of bins:"), # you use i18n object as always
-                  min = 1,
-                  max = 50,
-                  value = 30)
-    ),
-    mainPanel(
-      plotOutput("distPlot"),
-      p(i18n$t("This is description of the plot."))
-    )
-  )
+  titlePanel('shiny.i18n'),
+  uiOutput('page_content')
 )
+
+# Here we create our translator ...
+translator <- Translator$new(translation_json_path = "dados/translation_json/translations.json")
+translator$set_translation_language("pt")
 
 server <- function(input, output, session) {
   
-  observeEvent(input$selected_language, {
-    # This print is just for demonstration
-    print(paste("Language change!", input$selected_language))
-    # Here is where we update language in session
-    shiny.i18n::update_lang(session, input$selected_language)
+  # ... and here its reactive version that react to changes of the language.
+  i18n <- reactive({
+    selected <- input$selected_language
+    if (length(selected) > 0 && selected %in% translator$get_languages()) {
+      translator$set_translation_language(selected)
+    }
+    translator
   })
   
   output$distPlot <- renderPlot({
@@ -48,8 +32,35 @@ server <- function(input, output, session) {
     bins <- seq(min(x), max(x), length.out = input$bins + 1)
     hist(x, breaks = bins,
          col = "darkgray", border = "white",
-         main = i18n$t("Histogram of x"), ylab = i18n$t("Frequency"))
+         main = i18n()$t("Histogram of x"), ylab = i18n()$t("Frequency"))
   })
+  
+  output$ page_content <- renderUI({
+    tagList(
+      sidebarLayout(
+        sidebarPanel(
+          selectInput('selected_language',
+                      i18n()$t("Change language"),
+                      choices = translator$get_languages(),
+                      selected = input$selected_language),
+          sliderInput("bins",
+                      "Number of bins:",
+                      min = 1,
+                      max = 50,
+                      value = 30)
+        ),
+        mainPanel(
+          plotOutput("distPlot"),
+          p(i18n()$t("This is description of the plot."))
+        )
+      )
+    )
+  })
+  
+  observeEvent(i18n(), {
+    updateSliderInput(session, "bins", label =  i18n()$t("Number of bins:"), value = req(input$bins))
+  })
+  
 }
 
-shinyApp(ui, server)
+shinyApp(ui = ui, server = server)
